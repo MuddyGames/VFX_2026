f8cc6fa776c89fe7e4d707a6fe9ae7a70a15cdc6
MuddyGames <make@muddygames.com>
2026-09-15 11:57:48 +0100

    Added practical

---
+++ PRACTICAL_01/simple_function.py
@@ -0,0 +1,15 @@
+def add():
+	x = 1 + 3
+	return x
+
+
+def hello():
+	print("Hello There")
+
+def loopy():
+	for i in range(10):
+		print("Hello for the i'th time")
+
+print(add())
+hello()
+loopy()
